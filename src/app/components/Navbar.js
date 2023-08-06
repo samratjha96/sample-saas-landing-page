d343bb255ddc6b2fa5f5543478d2a570ba134ba2
Samrat Jha <samratj@amazon.com>
2023-08-05 22:15:14 -0400

    feat: Hero section

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,4 +1,3 @@
-"use client"
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
@@ -21,11 +20,11 @@ const Navbar = () => {
         <li className="p-4">Contact</li>
       </ul>
       <div onClick={toggle} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
 
       {/* Mobile view */}
-      <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
+      <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React</h1>
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+"use client"
 import Image from 'next/image'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
     <main>
       <div>
         <Navbar/>
+        <Hero/>
       </div>
     </main>
   )
